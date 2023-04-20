529fc4abe1d68fb1525b31998e56460a9faf05a6
kraujinis <kraujinis@gmail.com>
2023-04-20 19:17:47 +0300

    create 5 products

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Product:
     price: float
     quantity: int
 
-    products_list = []
+    products_list: list = []
 
     def total_cost(self):
         total_cost = self.price * self.quantity
@@ -30,3 +30,14 @@ class Product:
 
     def find_highest_total_cost(self):
         pass
+
+
+product_1 = Product(product_id="1", name="Tranzistorius", price=0.15, quantity=50)
+product_2 = Product(product_id="1", name="Rezistorius", price=1.15, quantity=50)
+product_3 = Product(product_id="1", name="Kondensatorius", price=0.75, quantity=50)
+product_4 = Product(product_id="1", name="Varistorius", price=2.15, quantity=50)
+product_5 = Product(product_id="1", name="Simistorius", price=0.85, quantity=50)
+
+Product.products_list(product_1, product_2, product_3, product_4, product_5)
+print(Product.products_list)
+print(product_1.total_cost())
